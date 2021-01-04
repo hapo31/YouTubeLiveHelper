99da6fef2ad50356d9f07c684ebf4d976d30a6ae
happou31 <happo31@gmail.com>
2021-01-05 00:39:21 +0900

    feat: add Menu

---
+++ src/components/Menu/Menu.stories.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { text, array, number } from "@storybook/addon-knobs";
+
+import Menu from "./Menu";
+import { Meta, Story } from "@storybook/react/types-6-0";
+
+const Template: Story<any> = args => <Menu {...args} />;
+
+export const Primary = Template.bind({});
+
+export default {
+  title: "Menu",
+  component: Menu,
+} as Meta;
+
+Primary.args = {
+  titles: array("Titles", ["page0", "page1"]),
+  page: number("Page", 0),
+  onClickPage: () => {},
+};
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+type Props = {
+  titles: string[];
+  page: number;
+  onClickPage: (index: number) => void;
+};
+
+export default (props: Props) => {
+  return (
+    <Container>
+      {props.titles.map((title, i) => (
+        <PageButton key={`page-btn-${i}`}>{title}</PageButton>
+      ))}
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  margin: 10px;
+  width: 100%;
+  height: 32px;
+`;
+
+const PageButton = styled.button`
+  margin: 5px 10px;
+  padding: 10px 15px;
+  border: solid 1px;
+  border-radius: 5px;
+`;
