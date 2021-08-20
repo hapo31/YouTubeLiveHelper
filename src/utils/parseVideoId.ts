05bc2fa9c35bb51120766d08473a024611fc5d04
happou31 <happo31@gmail.com>
2021-08-21 03:20:54 +0900

    fix: 余計な console.log

--- src/utils/parseVideoId.ts
+++ src/utils/parseVideoId.ts
@@ -5,7 +5,6 @@ export default function parseVideoId(url: string) {
   const match = url.match(
     /https:\/\/studio\.youtube\.com\/video\/(.+)\/livestreaming/
   );
-  console.log({ match });
   if (match?.[1]) {
     return match[1];
   } else {
