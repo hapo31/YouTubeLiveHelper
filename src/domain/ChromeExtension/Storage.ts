ba2b21f58373a72dae434534d1834827e388d4ec
happou31 <happo31@gmail.com>
2021-11-20 03:40:04 +0900

    chore: バックグラウンドでスパチャを取ってくる処理の準備

--- src/domain/ChromeExtension/Storage.ts
+++ src/domain/ChromeExtension/Storage.ts
@@ -1,3 +1,5 @@
+import { SuperChatInfo } from "../../state/AppState";
+
 export default function GetStorage<StoreType>(
   key: string
 ): Promise<StoreType | undefined> {
@@ -5,5 +7,18 @@ export default function GetStorage<StoreType>(
     chrome.storage.local.get((items) => {
       res(items[key] as StoreType);
     });
+  });
+}
+
+export function addSuperChat(superChatInfo: SuperChatInfo) {
+  chrome.storage.local.get("super_chat", (result) => {
+    let list = JSON.parse(result["super_chat"]) as SuperChatInfo[] | null;
+    if (!list) {
+      list = [];
+    }
+
+    list.push(superChatInfo);
+
+    chrome.storage.local.set({ super_chat: list });
   });
 }
